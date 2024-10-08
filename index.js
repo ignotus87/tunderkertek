934f8cc6977907239e57b775a36b703e980cabcb
Takacs, Zsolt <zsolt.takacs.ext@siemens-energy.com>
2024-10-08 08:37:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  const centerOfTheMapBudapest = { lat: 47.4810954, lng: 18.9654951 };
+	
+  // The locations
+  const locations = [
+  { position: { lat: 46.885316, lng: 16.4984948}, title: 'Szarvas József tündérkert' }
+  ];
+  
+  // Request needed libraries.
+  //@ts-ignore
+  // Request needed libraries.
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+    "marker",
+  );
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 7,
+    center: centerOfTheMapBudapest,
+    mapId: "DEMO_MAP_ID",
+  });
+  
+  function markerTicked(event) {
+	  if (event.type === "click") {
+		
+	  } else {
+		/* handle an error */
+	  }
+  }
+
+  // Create an info window to share between markers.
+  const infoWindow = new InfoWindow();
+	  
+  locations.forEach(({position, title}, i) => {
+	  
+	  const pin = new PinElement({
+		  scale: 1.5,
+	  });
+	  
+	  const marker = new AdvancedMarkerElement({
+		map: map,
+		position: position,
+		title: title,
+		content: pin.element,
+		gmpClickable: true
+	  });
+	  
+	  // Add a click listener for each marker, and set up the info window.
+	  marker.addListener("click", ({ domEvent, latLng }) => {
+		 const { target } = domEvent;
+
+		 infoWindow.close();
+		 infoWindow.setContent(marker.title);
+		 infoWindow.open(marker.map, marker);
+	  });
+  });
+}
+
+initMap();
